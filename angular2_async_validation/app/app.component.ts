e1531d879e2b2d21194c5e2b0f6ebce75a3fdba3
Matt T <MMT@Matts-MacBook-Pro.local>
2016-06-10 15:38:51 -0400

    adding form

--- angular2_async_validation/app/app.component.ts
+++ angular2_async_validation/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {LoginComponent} from './login.component';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    directives: [LoginComponent],
+    template: '<login></login>'
 })
 export class AppComponent { }
---
+++ angular2_async_validation/app/login.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: 'app/login.component.html'
+})
+export class LoginComponent{
+
+}
